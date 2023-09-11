95e6488b8306f94d4d2375e477bdca948bb79ac0
Devender Kumar Thakur <101546402+DevenderKThakur@users.noreply.github.com>
2023-09-11 13:56:53 +0700

    Update guessComp.py

--- guessComp.py
+++ guessComp.py
@@ -1,15 +1,22 @@
+
 import random 
 
-def guess(x):
-  random_number = random.randint(1,x)
-  guess = 0 
-  while guess != random_number :
-    guess = int(input(f'Enter the number from 1 to {x}: '))
-    if guess < random_number:
-      print ('Sorry the guess number is too low ')
-    elif guess > random_number :
-      print ('Sorry the guess number is too high ')
-  print (f'Congrats you have the guessed the correct number {random_number }')  
+def computer_guess(x):
+  low = 1 
+  high = x 
+  feedback = ''
+  while feedback != 'c':
+    if low != high :
+      guess = random.randint(low , high)
+    else :
+      guess = low 
+    feedback = input(f'Is {guess} is too high (H) , too low (l) or correct (c)').lower()
+    if feedback == 'h':
+      high = guess-1;
+    elif feedback == 'l':
+      low = guess + 1 
+  print (f'Yay Congrats the number is {guess}')
 
-guess(10)
+computer_guess(10)
+  
   
